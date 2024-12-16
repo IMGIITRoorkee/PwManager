a3ef9d0d80cfe12c347a9cc982837be4c9269c5b
adityapro3 <adityathetopper1@gmail.com>
2024-12-16 17:45:41 +0530

    Added my name in contributors.md and completed Improve error handling for missing password files #1

--- manager.py
+++ manager.py
@@ -25,10 +25,13 @@ class PasswordManager:
 
     def load_password_file(self, path):
         self.password_file = path
-        with open(path, 'r') as f:
-            for line in f:
-                site, encrypted = line.split(":")
-                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+        try:
+            with open(path, 'r') as f:
+                for line in f:
+                    site, encrypted = line.split(":")
+                    self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+        except:
+            print("Sorry, But looks like there is no file")
 
     def add_password(self, site, password):
         self.password_dict[site] = password
