fbd13a5d4c7500f93fba2159914f453f633dc322
anshkarwasra <anshkarwasra15@gmail.com>
2024-12-16 19:06:21 +0530

    final commit

--- manager.py
+++ manager.py
@@ -1,5 +1,4 @@
 from cryptography.fernet import Fernet
-from datetime import datetime
 
 class PasswordManager:
 
@@ -12,10 +11,7 @@ class PasswordManager:
         self.key = Fernet.generate_key()
         with open(path, 'wb') as f:
             f.write(self.key)
-    def showDate(self):
-        if self.password_file is not None:
-            if self.password_dict.get("last_updated") is not None:
-                print("Last updated: " + self.password_dict.get("last_updated"))
+    
 
     def load_key(self, path):
         with open(path, 'rb') as f:
@@ -23,9 +19,6 @@ class PasswordManager:
 
     def create_password_file(self, path, initial_values=None):
         self.password_file = path
-        with open(self.password_file,"a+") as f:
-            encryptedDate = Fernet(self.key).encrypt(datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode()).decode()
-            f.write(f"last_updated:{encryptedDate}\n")
         if initial_values is not None:
             for site, password in initial_values.items():
                 self.add_password(site, password)
@@ -48,4 +41,4 @@ class PasswordManager:
                 
 
     def get_password(self, site):
-        return "the password for " + site + " is "+  self.password_dict.get(site, "Password not found.") + " and was Last updated: " + self.password_dict.get("last_updated", "date not found")
+        return self.password_dict.get(site, "Password not found.")
