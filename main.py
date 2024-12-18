8ef03980aae9c010094a9c41d1f2573df1735c5c
anshkarwasra <anshkarwasra15@gmail.com>
2024-12-18 10:32:31 +0530

    final gui version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk,filedialog,messagebox
 from manager import PasswordManager
 
 password = {
@@ -8,13 +8,34 @@ password = {
         "twitter": "password3"
     }
 
-
 pm = PasswordManager()
 root = Tk()
-
-pathVar = StringVar()
 siteVar = StringVar()
 passVar = StringVar()
+
+
+
+def save_file(filetypes):
+        # File save dialog
+        filename = filedialog.asksaveasfilename(
+            defaultextension=".key",
+            filetypes=[
+                filetypes
+            ]
+        )
+
+        if filename:
+            return filename
+def selectDirectory(fileName,filetypes):
+    filename = filedialog.askopenfilename(
+            title=f"Open a {fileName} file",
+            initialdir="/",
+            filetypes=[
+                filetypes
+            ]
+        )
+    if filename:
+        return filename
 
 def remove_widget(row, column):
     for widget in root.grid_slaves():  
@@ -23,77 +44,78 @@ def remove_widget(row, column):
             break
 
 def makeKey():
-    pm.create_key(pathVar.get())
+    filename = save_file(("key Files", "*.key"))
+    pm.create_key(filename)
+    messagebox.showinfo("Key saved", f"Key saved to {filename}")
 
 def loadKey():    
-    pm.load_key(pathVar.get())
+    path = selectDirectory("key",("key Files", "*.key"))
+    print(path)
+    pm.load_key(path)
+    messagebox.showinfo("Key loaded", f"Key loaded from {path}")
 
 def makePassFile():
-    pm.create_password_file(pathVar.get(),password)
+    fileName = save_file(("password file", "*.txt"))
+    pm.create_password_file(fileName,password)
+    messagebox.showinfo("Password file created", f"Password file created at {fileName}")
 
 def loadPassFile():
-    pm.load_password_file(pathVar.get())
+    path = selectDirectory("password file",("password file", "*.txt"))
+    pm.load_password_file(path=path)
+    messagebox.showinfo("Password file loaded", f"Password file loaded from {path}")
 
 def createPass():
     pm.add_password(siteVar.get(),passVar.get())
+    messagebox.showinfo("Password created", f"Password created for {siteVar.get()}")
 
 def getPass():
-    siteLabel = ttk.Label(root,text=f"password for {str(siteVar.get())}: {pm.get_password(siteVar.get())}")
-    siteLabel.grid(column=0,row=3)
+    messagebox.showinfo("Password retrieved", f"Password retrieved for {siteVar.get()} and the password is {pm.get_password(siteVar.get())}")
+
 
 def pmManager():
 
-    for col in range(3):
-        print(col)
-        remove_widget(2,col)
+    for col in range(4):
         remove_widget(3,col)
+        remove_widget(4,col)
     choice = choiceVar.get()
     if choice == '1':
-        pathLabel = ttk.Label(root,text="Enter key file path: ")
-        pathEntry = ttk.Entry(root,textvariable=pathVar)
-        pathButton = ttk.Button(root,text="submit",command=makeKey)
-        pathLabel.grid(column=0,row=2)
-        pathEntry.grid(column=1,row=2)
-        pathButton.grid(column=2,row=2)
+        pathLabel = ttk.Label(root,text="save your key file: ")
+        pathButton = ttk.Button(root,text="save",command=makeKey)
+        pathLabel.grid(column=0,row=3)
+        pathButton.grid(column=1,row=3)
     elif choice == '2':
-        pathLabel = ttk.Label(root,text="Enter key file path: ")
-        pathEntry = ttk.Entry(root,textvariable=pathVar)
-        pathButton = ttk.Button(root,text="submit",command=loadKey)
-        pathLabel.grid(column=0,row=2)
-        pathEntry.grid(column=1,row=2)
-        pathButton.grid(column=2,row=2)
+        pathLabel = ttk.Label(root,text="select key file : ")
+        pathButton = ttk.Button(root,text="select",command=loadKey)
+        pathLabel.grid(column=0,row=3)
+        pathButton.grid(column=1,row=3)
     elif choice == '3':
-        pathLabel = ttk.Label(root,text="Enter password file path: ")
-        pathEntry = ttk.Entry(root,textvariable=pathVar)
-        pathButton = ttk.Button(root,text="submit",command=makePassFile)
-        pathLabel.grid(column=0,row=2)
-        pathEntry.grid(column=1,row=2)
-        pathButton.grid(column=2,row=2)
+        pathLabel = ttk.Label(root,text="make a password file: ")
+        pathButton = ttk.Button(root,text="make",command=makePassFile)
+        pathLabel.grid(column=0,row=3)
+        pathButton.grid(column=1,row=3)
     elif choice == '4':
-        pathLabel = ttk.Label(root,text="Enter password file path: ")
-        pathEntry = ttk.Entry(root,textvariable=pathVar)
-        pathButton = ttk.Button(root,text="submit",command=loadPassFile)
-        pathLabel.grid(column=0,row=2)
-        pathEntry.grid(column=1,row=2)
-        pathButton.grid(column=2,row=2)
+        pathLabel = ttk.Label(root,text="select password file: ")
+        pathButton = ttk.Button(root,text="select",command=loadPassFile)
+        pathLabel.grid(column=0,row=3)
+        pathButton.grid(column=1,row=3)
     elif choice == '5':
         siteLabel = ttk.Label(root,text="Enter site: ")
         siteEntry = ttk.Entry(root,textvariable=siteVar)
-        siteLabel.grid(column=0,row=2)
-        siteEntry.grid(column=1,row=2)
+        siteLabel.grid(column=0,row=3)
+        siteEntry.grid(column=1,row=3)
         passwordLabel = ttk.Label(root,text="Enter password: ")
         passwordEntry = ttk.Entry(root,textvariable=passVar)
         passButton = ttk.Button(root,text="submit",command=createPass)
-        passwordLabel.grid(column=0,row=3)
-        passwordEntry.grid(column=1,row=3)
-        passButton.grid(column=2,row=3)   
+        passwordLabel.grid(column=0,row=4)
+        passwordEntry.grid(column=1,row=4)
+        passButton.grid(column=2,row=4)   
     elif choice == '6':
         siteLabel = ttk.Label(root,text="Enter site: ")
         siteEntry = ttk.Entry(root,textvariable=siteVar)
         siteButton = ttk.Button(root,text="submit",command=getPass)
-        siteLabel.grid(column=0,row=2)
-        siteEntry.grid(column=1,row=2)
-        siteButton.grid(column=2,row=2)
+        siteLabel.grid(column=0,row=3)
+        siteEntry.grid(column=1,row=3)
+        siteButton.grid(column=2,row=3)
     else:
         pass
 
@@ -102,13 +124,15 @@ def pmManager():
 
 frm = ttk.Frame(root,padding=10)
 frm.grid()
+root.title("Password Manager")
 
 choiceVar = StringVar()
 
-# ttk.Label(frm,text="Hello World!").grid(column=0,row=0)
-ttk.Button(frm,text="Exit",command=root.destroy).grid(column=1,row=0)
-ttk.Button(frm,text="Choice",command=pmManager).grid(column=0,row=0)
+ttk.Label(frm,text="Welcome to the Password Manager").grid(column=0,row=0)
+ttk.Button(frm,text="Exit",command=root.destroy).grid(column=0,row=2)
+ttk.Button(frm,text="Choice",command=pmManager).grid(column=0,row=1)
 choiceLabel = ttk.Entry(root,textvariable=choiceVar)
-choiceLabel.grid(column=0,row=1)
+choiceLabel.grid(column=0,row=2)
 
+print(TkVersion,TclVersion)
 root.mainloop()
