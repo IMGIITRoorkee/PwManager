2ccb371cdce22f560d96602660300da3e99979ac
ansh karwasra <anshkarwasra15@gmail.com>
2024-12-18 10:53:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from manager import PasswordManager
+import pyperclip
 import asyncio
-import sys
+import os
 
 
 async def setTimeOut(prompt, timeout):
@@ -10,10 +11,15 @@ async def setTimeOut(prompt, timeout):
         return userInp
     except asyncio.TimeoutError:
         print("\nTimeout. Goodbye!")
-        import os
         # Use os._exit to forcefully terminate the process
         os._exit(0)
-    
+
+def validate_key_loaded(pm : PasswordManager):
+    if not pm.keyloaded:
+        print("Key not loaded. Please load a key first.")
+        return False
+    return True
+
 async def main():
     password = {
         "gmail": "password1",
@@ -21,6 +27,8 @@ async def main():
         "twitter": "password3"
     }
     
+    pm = PasswordManager()
+
     print("""What would you like to do?
           1. Create a new key
           2. Load an existing key
@@ -28,37 +36,69 @@ async def main():
           4. Load an existing password file
           5. Add a password
           6. Get a password
+          7. List all sites
           q. Quit
           """)
-
+    
     done = False
-    pm = PasswordManager()
     while not done:
-        choice = await setTimeOut("Enter your choice: ", 10)
+        choice = await setTimeOut("Enter choice: ", 30)
+        choice = choice.strip().lower()
+
         if choice == '1':
-            path = await setTimeOut("Enter key file path: ", 10)
+            path = await setTimeOut("Enter key file path: ", 30)
+            path = path.strip()
             pm.create_key(path)
+
         elif choice == '2':
-            path = await setTimeOut("Enter key file path: ", 10)
+            path = await setTimeOut("Enter key file path: ", 30)
+            path = path.strip()
             pm.load_key(path)
-        elif choice == '3':
-            path = await setTimeOut("Enter password file path: ", 10)
+
+        elif choice == '3' and validate_key_loaded(pm):
+            path = await setTimeOut("Enter password file path: ", 30)
+            path = path.strip()
             pm.create_password_file(path, password)
-        elif choice == '4':
-            path = await setTimeOut("Enter password file path: ", 10)
+
+        elif choice == '4' and validate_key_loaded(pm):
+            path = await setTimeOut("Enter password file path: ", 30)
+            path = path.strip()
             pm.load_password_file(path)
-        elif choice == '5':
-            site = await setTimeOut("Enter site: ", 10)
-            password = await setTimeOut("Enter password: ", 10)
+
+        elif choice == '5' and validate_key_loaded(pm):
+            site = await setTimeOut("Enter site: ", 30)
+            site = site.strip()
+            password = await setTimeOut("Enter password: ", 30)
+            password = password.strip()
+            if pm.validate_strength(password):
+                print("added successfully")
+            else:
+                print("WARNING: This password is weak, It is recommended to set a stronger password")
+                print("- Password should be more than 8 characters long")
+                print("- Password should have alphanumeric characters, capital letters and special characters")
             pm.add_password(site, password)
-        elif choice == '6':
-            site = await setTimeOut("Enter site: ", 10)
-            print(f"Password for {site}: {pm.get_password(site)}")
+
+        elif choice == '6' and validate_key_loaded(pm):
+            site = await setTimeOut("Enter site: ", 30)
+            site = site.strip()
+            res = pm.get_password(site)
+            print(f"Password for {site}: {res}")
+            if res != "Password not found.":
+                pyperclip.copy(pm.get_password(site))
+                print("Password copied to clipboard.")
+
+        elif choice == '7':
+            print("Saved Sites:")
+            for site in pm.password_dict:
+                print(site)
+
         elif choice == 'q':
             done = True
             print("Goodbye!")
+
         else:
             print("Invalid choice. Please try again.")
+
+
 if __name__ == '__main__':
-    main()
     asyncio.run(main())
